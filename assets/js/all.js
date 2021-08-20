ea0d60f348d5d19a32ca4d421314969c2c932a67
Hua_Nan <x565619@gmail.com>
2021-08-21 02:49:48 +0800

    Update 2021-08-20T18:49:43.156Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,27 +1,29 @@
+"use strict";
+
 var courseSwiper = new Swiper(".courseSwiper", {
   slidesPerView: 1.5,
-  spaceBetween: 30,
+  spaceBetween: 30
 });
-
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   slidesPerColumn: 3,
   spaceBetween: 30,
   loop: false,
   breakpoints: {
-    768:{
+    768: {
       slidesPerView: 2,
       slidesPerColumn: 2,
-      spaceBetween: 30,
+      spaceBetween: 30
     },
-    992:{
+    992: {
       slidesPerView: 3,
       slidesPerColumn: 1,
-      spaceBetween: 30,
-    },
+      spaceBetween: 30
+    }
   },
   navigation: {
     nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    prevEl: ".swiper-button-prev"
+  }
 });
+//# sourceMappingURL=all.js.map
